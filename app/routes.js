7bd32503507e04cba664864c0c4c1c0e5dd32f7b
Jesse Yuen <hello@jesseyuen.com>
2020-07-28 21:54:05 +0100

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+// Success message
+// router.post('/examples/success-errors', function (req, res) {
+//     res.render('examples/success-errors/answer', { showMessage: true })
+// })
+
+module.exports = router
